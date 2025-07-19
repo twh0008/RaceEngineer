dc9223e13d03c8febd721e9ba26f74ca72bbb224
boredTabard <nathanlvtt9+github@gmail.com>
2025-07-19 12:58:04 -0700

    es-lint and prettier JS file checks

--- packages/dev-mode.js
+++ packages/dev-mode.js
@@ -1,4 +1,4 @@
-import {build, createServer} from 'vite';
+import { build, createServer } from 'vite';
 import path from 'path';
 
 /**
@@ -6,14 +6,12 @@ import path from 'path';
  * To do this, you need to follow a few steps:
  */
 
-
 /**
  * 1. We create a few flags to let everyone know that we are in development mode.
  */
 const mode = 'development';
 process.env.NODE_ENV = mode;
 process.env.MODE = mode;
-
 
 /**
  * 2. We create a development server for the renderer. It is assumed that the renderer exists and is located in the “renderer” package.
@@ -29,7 +27,6 @@ const rendererWatchServer = await createServer({
 
 await rendererWatchServer.listen();
 
-
 /**
  * 3. We are creating a simple provider plugin.
  * Its only purpose is to provide access to the renderer dev-server to all other build processes.
@@ -44,24 +41,18 @@ const rendererWatchServerProvider = {
   },
 };
 
-
 /**
  * 4. Start building all other packages.
  * For each of them, we add a plugin provider so that each package can implement its own hot update mechanism.
  */
 
 /** @type {string[]} */
-const packagesToStart = [
-  'packages/preload',
-  'packages/main',
-];
+const packagesToStart = ['packages/preload', 'packages/main'];
 
 for (const pkg of packagesToStart) {
   await build({
     mode,
     root: path.resolve(pkg),
-    plugins: [
-      rendererWatchServerProvider,
-    ],
+    plugins: [rendererWatchServerProvider],
   });
 }
